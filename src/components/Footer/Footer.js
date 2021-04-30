76449ad6865b891584b4e0a9e46d3485f8b736b9
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-04-30 20:58:09 +0500

    edit

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -11,34 +11,34 @@ const Footer = () => {
           <div className={style.sections}>
             <h3>Sahifalar</h3>
             <div className={style.links}>
-              <Link to='/'>Filial</Link>
-              <Link to='/'>Tuzilma</Link>
-              <Link to='/'>Ta'lim</Link>
+              <Link to="/filial">Filial</Link>
+              <Link to="/tuzilma">Tuzilma</Link>
+              <Link to="/talim">Ta'lim</Link>
             </div>
           </div>
           <div className={style.sections}>
             <h3>Tuzilmalar</h3>
             <div className={style.links}>
-              <Link to='/'>Kafedralar</Link>
-              <Link to='/'>Bo'limlar</Link>
-              <Link to='/'>Dekanatlar</Link>
+              <Link to="/kafedralar">Kafedralar</Link>
+              <Link to="/bolimlar">Bo'limlar</Link>
+              <Link to="/dekanatlar">Dekanatlar</Link>
             </div>
           </div>
           <div className={style.sections}>
             <h3>Boshqa</h3>
             <div className={style.links}>
-              <Link to='/'>Yangiliklar</Link>
-              <Link to='/'>Qabdiv</Link>
-              <Link to='/'>Aloqa</Link>
+              <Link to="/yangiliklar">Yangiliklar</Link>
+              <Link to="/qabul">Qabul</Link>
+              <Link to="/aloqa">Aloqa</Link>
             </div>
           </div>
         </div>
         <div className={style.blogLinks}>
-          <Link to='/'>Facebook</Link>
-          <Link to='/'>Telegram</Link>
-          <img width='110px' src={logoFooter} alt="logoFooter" />
-          <Link to='/'>Youtube</Link>
-          <Link to='/'>Instagram</Link>
+          <Link to="/">Facebook</Link>
+          <Link to="/">Telegram</Link>
+          <img width="110px" src={logoFooter} alt="logoFooter" />
+          <Link to="/">Youtube</Link>
+          <Link to="/">Instagram</Link>
         </div>
         <p className={style.copy}>
           Copyright ©2021 TDYU Ixtisoslashtirilgan filiali. All right Reserved
