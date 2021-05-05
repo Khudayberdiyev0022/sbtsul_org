d455f08914602ed0fae89af76f0373751e491a70
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-05-05 17:46:58 +0500

    rdghljlk

--- src/componentsRUS/Home/Home.js
+++ src/componentsRUS/Home/Home.js
@@ -5,7 +5,7 @@ import Home4 from "./HomeParts/Home4";
 import Home5 from "./HomeParts/Home5";
 import Home6 from "./HomeParts/Home6";
 import Home7 from "./HomeParts/Home7";
-import Home8 from "./HomeParts/Home8";
+// import Home8 from "./HomeParts/Home8";
 import Home9 from "./HomeParts/Home9";
 
 const HomeRus = () => {
@@ -17,7 +17,7 @@ const HomeRus = () => {
       <Home4 />
       <Home5 />
       <Home6 />
-      <Home8 />
+      {/* <Home8 /> */}
       <Home9 />
     </div>
   );
