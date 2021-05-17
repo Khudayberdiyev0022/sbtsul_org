f3cbfe24d1044716383c5e3e97b8ac0a34f66767
Khudayberdiyev0022 <khamza.khudayberdiyev@gmail.com>
2021-05-17 11:22:44 +0500

    2

--- src/components/Home/HomeParts/Home2.js
+++ src/components/Home/HomeParts/Home2.js
@@ -1,38 +1,37 @@
 import React, { useEffect } from "react";
 import style from "./Home2.module.css";
 import president from "../../../assets/img/pr.webp";
-import AOS from 'aos'
-import 'aos/dist/aos.css';
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 const Home2 = () => {
-
   useEffect(() => {
-   AOS.init()
-  })
+    AOS.init();
+  });
 
   return (
     <div className={style.main} data-aos="fade-up-right">
       <div className={style.container}>
-          <div className={style.textPresident} >
-            <h2 data-aos="fade-down">
-              O'ZBEKISTON RESPUBLIKASI PREZIDENTINING 2019 YIL 7 FEVRALDAGI{" "}
-              <br /> PQ–4166 - SON QARORI
-            </h2>
-            <h3 data-aos="fade-up">Ixtisoslashtirilgan filial tashkil etildi</h3>
-          </div>
+        <div className={style.textPresident}>
+          <h2 data-aos="fade-down">
+            O'ZBEKISTON RESPUBLIKASI PREZIDENTINING 2019 YIL 7 FEVRALDAGI <br />{" "}
+            PQ–4166 - SON QARORI
+          </h2>
+          <h3 data-aos="fade-up">Ixtisoslashtirilgan filial tashkil etildi</h3>
+        </div>
         <div className={style.bgAll}>
           <div
             className={style.leftImg}
             style={{ backgroundImage: `url(${president})` }}
           ></div>
-          <div className={style.rightCenter} >
+          <div className={style.rightCenter}>
             <p data-aos="fade-down">
               O'zbekiston Respublikasi Prezidentining 07.02.2019 yildagi
-              “Huqubuzarliklar profilaktikasi va jamoa xavfsizligi ta'minlash
-              sohasida kadrlar tayyorlash tizimini takomillashtirishga doir
-              qo'shimcha chora-tadbirlar to'g'risida”gi PQ–4166-son qarori bilan
-              Toshkent davlat yuridik universitetining Ixtisoslashtirilgan
-              filiali tashkil etilgan.
+              “Huquqbuzarliklar profilaktikasi va jamoat xavfsizligini
+              ta'minlash sohasida kadrlar tayyorlash tizimini
+              takomillashtirishga doir qo'shimcha chora-tadbirlar to'g'risida”gi
+              PQ–4166-son qarori bilan Toshkent davlat yuridik universitetining
+              Ixtisoslashtirilgan filiali tashkil etilgan.
             </p>
           </div>
           <div className={style.rightText}></div>
